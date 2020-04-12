bb4b817500edcef05b36f79ad464ede92cbdc31a
lukasaristide <luk2002luk@gmail.com>
2020-04-12 20:45:59 +0200

    First glance at Graphic class

--- shiny-potato/src/shiny_potatoes/Graphic.java
+++ shiny-potato/src/shiny_potatoes/Graphic.java
@@ -1,5 +1,25 @@
 package shiny_potatoes;
 
-public class Graphic {
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL;
 
+public class Graphic extends Thread {
+	private Logic resource;
+
+	void initializeEverything() {
+		// making this window current on this Thread
+		GLFW.glfwMakeContextCurrent(resource.window);
+		// creating capabilities for the current thread
+		GL.createCapabilities();
+	}
+
+	@Override
+	public void run() {
+		initializeEverything();
+		
+	}
+
+	Graphic(Logic Resource) {
+		this.resource = Resource;
+	}
 }
