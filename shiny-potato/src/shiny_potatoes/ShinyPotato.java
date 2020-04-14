03ce511061573fd5a8ad18da460ebc31e94565ea
lukasaristide <luk2002luk@gmail.com>
2020-04-15 00:52:42 +0200

    written basic form of it - needed to test graphic

--- shiny-potato/src/shiny_potatoes/ShinyPotato.java
+++ shiny-potato/src/shiny_potatoes/ShinyPotato.java
@@ -1,7 +1,22 @@
 package shiny_potatoes;
 
+//import org.lwjgl.glfw.GLFW;	code for testing display
+
 public class ShinyPotato {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		//creation of the whole structure
+		Logic logic = new Logic();
+		@SuppressWarnings("unused")
+		Interface interfac = new Interface(logic);
+		Graphic graphic = new Graphic(logic);
+		//start of the graphic
+		graphic.start();
 		
+		/* code for testing display
+		while(!GLFW.glfwWindowShouldClose(logic.window))
+			GLFW.glfwWaitEvents();
+		GLFW.glfwDestroyWindow(logic.window);
+		GLFW.glfwTerminate();
+		*/
 	}
 }
