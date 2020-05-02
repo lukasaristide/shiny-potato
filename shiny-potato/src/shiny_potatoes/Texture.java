dbf7894ec42083523b6e568ba6a6cd7567374f4d
lukasaristide <luk2002luk@gmail.com>
2020-05-02 16:48:08 +0200

    textures now do work

--- shiny-potato/src/shiny_potatoes/Texture.java
+++ shiny-potato/src/shiny_potatoes/Texture.java
@@ -5,16 +5,15 @@ import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL46;
+import static org.lwjgl.opengl.GL46.*;
 import org.lwjgl.stb.STBImage;
-
 
 public class Texture{
 	ByteBuffer image;
 	int id;
 	int width, height;
 	void bind() {
-		GL46.glBindTexture(GL46.GL_TEXTURE_2D, id);
+		glBindTexture(GL_TEXTURE_2D, id);
 	}
 	public Texture() {}
 	Texture(String filename) throws IOException{
@@ -22,16 +21,20 @@ public class Texture{
 		IntBuffer height = BufferUtils.createIntBuffer(1);
 		IntBuffer comp = BufferUtils.createIntBuffer(1);
 		
-		image = STBImage.stbi_load(filename, width, height, comp, 4);
+		image = STBImage.stbi_load(filename, width, height, comp, 0);
 		
-		id = GL46.glGenTextures();
-		this.width = width.get();
-		this.height = height.get();
+		id = glGenTextures();
+		id = glGenTextures();
+		id = glGenTextures();
+		
+		this.width = width.get(0);
+		this.height = height.get(0);
 		bind();
-		GL46.glTexParameteri(GL46.GL_TEXTURE_2D, GL46.GL_TEXTURE_WRAP_S, GL46.GL_CLAMP_TO_EDGE);
-		GL46.glTexParameteri(GL46.GL_TEXTURE_2D, GL46.GL_TEXTURE_WRAP_T, GL46.GL_CLAMP_TO_EDGE);
-		GL46.glTexParameteri(GL46.GL_TEXTURE_2D, GL46.GL_TEXTURE_MAG_FILTER, GL46.GL_LINEAR);
-		GL46.glTexParameteri(GL46.GL_TEXTURE_2D, GL46.GL_TEXTURE_MIN_FILTER, GL46.GL_LINEAR);
-		GL46.glTexImage2D(GL46.GL_TEXTURE_2D, 0, GL46.GL_RGBA, this.width, this.height, 0, GL46.GL_RGBA, GL46.GL_UNSIGNED_BYTE, image);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this.width, this.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 }
